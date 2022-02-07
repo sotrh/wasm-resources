213bf67abaa2c08c8f91ae4556b1ae58f516cee8
Ben Hansen <bhbenjaminhansen@gmail.com>
2022-02-07 09:56:07 -0700

    error reproduced

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::io::{BufReader, Cursor};
+
 #[cfg(target_arch="wasm32")]
 use wasm_bindgen::prelude::*;
 
@@ -10,7 +12,7 @@ cfg_if::cfg_if! {
                     if #[cfg(feature="gh_pages")] {
                         "https://sotrh.github.io/wasm-resources/res/".parse().unwrap()
                     } else {
-                        "http://127.0.0.1:8000/res/".parse().unwrap()
+                        "http://127.0.0.1:3000/res/".parse().unwrap()
                     }
                 }
             };
@@ -38,6 +40,40 @@ pub async fn fetch_text_file(res_name: &str) -> anyhow::Result<String> {
     Ok(text)
 }
 
+pub async fn fetch_binary_file(res_name: &str) -> anyhow::Result<Vec<u8>> {
+    cfg_if::cfg_if! {
+        if #[cfg(target_arch="wasm32")] {
+            let url = BASE_URL.join(res_name)?;
+            let res = reqwest::get(url).await?;
+            let data = res.bytes().await?.to_vec();
+        } else {
+            let path = BASE_PATH.join(res_name);
+            let data = std::fs::read(path)?;
+        }
+    }
+    Ok(data)
+}
+
+pub async fn fetch_obj(res_name: &str) -> anyhow::Result<(Vec<tobj::Model>, Vec<tobj::Material>)> {
+    let obj_text = fetch_text_file(res_name).await?;
+    let mut obj_reader = BufReader::new(Cursor::new(obj_text));
+    let (models, materials) = tobj::load_obj_buf(
+        &mut obj_reader,
+        &tobj::LoadOptions {
+            triangulate: true,
+            single_index: true,
+            ..Default::default()
+        },
+        |p| {
+            let p = p.to_str().ok_or(tobj::LoadError::OpenFileFailed)?;
+            let mtl_text = pollster::block_on(fetch_text_file(p)).unwrap();
+            tobj::load_mtl_buf(&mut BufReader::new(Cursor::new(mtl_text)))
+        }
+    )?;
+
+    Ok((models, materials?))
+}
+
 #[cfg(target_arch="wasm32")]
 #[cfg_attr(target_arch="wasm32", wasm_bindgen(start))]
 pub async fn start() {
@@ -47,4 +83,8 @@ pub async fn start() {
     // Not the most useful demo, but it gets the point across
     let text = fetch_text_file("wasm-tree.txt").await.unwrap();
     log::info!("Contents of 'wasm-tree.txt':\n\n{}", text);
+
+    let (models, materials) = fetch_obj("cube.obj").await.unwrap();
+    log::info!("Models for 'cube.obj':\n\n{:?}", models);
+    log::info!("Materials for 'cube.obj':\n\n{:?}", materials);
 }
